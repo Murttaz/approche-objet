c26d235d9cf80028bd568e4afda0a721f83beb35
Murttaz <vincentfournier1993@gmail.com>
2022-10-11 17:13:04 +0200

    essai exemple cours

---
+++ src/herite/Avion.java
@@ -0,0 +1,20 @@
+package herite;
+
+public class Avion extends Vehicule{
+
+	private String matricule;
+	
+	public void decoler() {
+		
+	}
+
+	public String getMatricule() {
+		return matricule;
+	}
+
+	public void setMatricule(String matricule) {
+		this.matricule = matricule;
+	}
+	
+}
+
---
+++ src/herite/Vehicule.java
@@ -0,0 +1,26 @@
+package herite;
+
+public class Vehicule {
+	
+	private String marque;
+	private String modele;
+	
+	public void demarrer() {
+	}
+		public String getMarque() {
+	
+		return marque;
+		}
+	public void setMarque(String marque) {
+		this.marque = marque;
+	}
+	public String getModele() {
+		return modele;
+	}
+	public void setModele(String modele) {
+		this.modele = modele;
+	}
+	
+	
+
+}
