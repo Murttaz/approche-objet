05b772ca8c5a5ce902095a14eaa1dbc436881f63
Murttaz <vincentfournier1993@gmail.com>
2022-10-11 15:52:30 +0200

    tp5 rendu

---
+++ src/fr/diginamic/banque/entites/TestBanque.java
@@ -0,0 +1,11 @@
+package fr.diginamic.banque.entites;
+
+public class TestBanque {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Compte c1= new Compte(142857,1587.23);
+		System.out.println(c1);
+	}
+
+}
